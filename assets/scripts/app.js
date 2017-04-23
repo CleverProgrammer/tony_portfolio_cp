f2e6c348738edac06e26eba1df6e5a950f3372cc
developertenzin <tenzinphuljung7@gmail.com>
2017-04-23 02:34:20 -0500

    Add typed animation. Add default my-button class.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,10 @@
-$("button").click(function() {
-	alert("hello");
-})
+$(function(){
+	Typed.new("#writing-text", {
+		strings: ["am a Web Developer", "am a micro-specialist & macro-generalist", "am a problem solver.", "am always learning new things... ^ALWAYS!"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 0,
+		contentType: 'text'
+	});
+});
