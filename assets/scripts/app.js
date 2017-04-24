e65c93f56dca69e1bdbc62faa90d269feb744769
developertenzin <tenzinphuljung7@gmail.com>
2017-04-24 04:10:29 -0500

    Finalize word cloud. Fix margin for word cloud.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,31 +1,35 @@
-	function makeWordCloud() {
+	function makeWords() {
 		var words = [
-  {text: "html3", weight: 13},
-  {text: "css5", weight: 10.5},
-  {text: "css3", weight: 9.4},
+  {text: "html5", weight: 12.3},
+  {text: "css3", weight: 12.5},
   {text: "javascript", weight: 13},
-  {text: "jquery", weight: 6.2},
-  {text: "programming", weight: 5},
-  {text: "python", weight: 5},
-	{text: "java", weight: 13},
-  {text: "nodejs", weight: 10.5},
-  {text: "npm", weight: 9.4},
-  {text: "web development", weight: 8},
-  {text: "C", weight: 6.2},
-  {text: "Scheme", weight: 5},
-  {text: "php", weight: 5},
-	{text: "hadoop", weight: 13},
-  {text: "numpy", weight: 10.5},
-  {text: "matplotlib", weight: 9.4},
+  {text: "jquery", weight: 12},
+  {text: "programming", weight: 10},
+  {text: "python", weight: 10},
+	{text: "java", weight: 9},
+  {text: "nodejs", weight: 11},
+  {text: "npm", weight: 9},
+  {text: "web development", weight: 10},
+  {text: "C", weight: 8.3},
+  {text: "Scheme", weight: 8.1},
+  {text: "php", weight: 7.8},
+	{text: "hadoop", weight: 8.5},
+  {text: "numpy", weight: 7},
+  {text: "matplotlib", weight: 7},
   {text: "ruby", weight: 8},
-  {text: "express", weight: 6.2},
-  {text: "flask", weight: 5},
-  {text: "responsive design", weight: 5},
-  {text: "bootstrap", weight: 5}
+  {text: "express", weight: 9},
+  {text: "flask", weight: 8.9},
+  {text: "responsive design", weight: 10},
+  {text: "bootstrap", weight: 10}
   /* ... */
 ];
+	return words;
+	}
 
-		$('.teaching-domains').jQCloud(words);
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {
+			delay: 100
+		});
 	}
 
 	function displayWordCloud() {
@@ -33,8 +37,9 @@
 		$(window).on('scroll', function() {
 			var y_scroll_pos = window.pageYOffset;
 			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
 			if (y_scroll_pos > scroll_pos_test && count <= 1) {
-				makeWordCloud();
+				makeWordCloud(words);
 				count++;
 			}
 		});
@@ -51,4 +56,10 @@
 		});
 
 		displayWordCloud();
+
+
+	})
+	$("#teaching").on("click", ".teaching-domains", function() {
+		console.log("yo");
+		$('#teaching-domains').load(document.URL +  ' #teaching');
 	})
