3a0a085ba535ada326363d894038d4b4d16b7144
developertenzin <tenzinphuljung7@gmail.com>
2017-04-24 04:24:16 -0500

    Add document.ready function. Remove click trigger on word cloud.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -45,21 +45,15 @@
 		});
 	}
 
-	$(function() {
-		Typed.new("#writing-text", {
-			strings: ["am a Web Developer", "am a micro-specialist & macro-generalist", "am a problem solver.", "am always learning new things... ^ALWAYS!"],
-			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-			stringsElement: null,
-			// typing speed
-			typeSpeed: 0,
-			contentType: 'text'
-		});
+		$(document).ready(function() {
+			Typed.new("#writing-text", {
+				strings: ["am a Web Developer", "am a micro-specialist & macro-generalist", "am a problem solver.", "am always learning new things... ^ALWAYS!"],
+				// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+				stringsElement: null,
+				// typing speed
+				typeSpeed: 0,
+				contentType: 'text'
+			});
 
-		displayWordCloud();
-
-
-	})
-	$("#teaching").on("click", ".teaching-domains", function() {
-		console.log("yo");
-		$('#teaching-domains').load(document.URL +  ' #teaching');
-	})
+			displayWordCloud();
+		})
